3875b6eb9d0d1e40fb94e2db349466ecd261edb6
Joachim Naulet <jnaulet@rdinnovation.fr>
2016-03-14 17:38:16 +0100

    Added Qualcomm mbn file support

---
+++ mbn.c
@@ -0,0 +1,72 @@
+/*
+ * mbn.c
+ * dloadtool
+ *
+ * Created by Joachim Naulet on 3/14/2016
+ *
+ * Copyright (c) 2016 R&D Innovation. All rights reserved
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "mbn.h"
+
+
+int mbn_open(const char *path, mbn_head_t *head) {
+
+  int fd;
+  uint32_t buf;
+  
+  if((fd = open(path, O_RDONLY)) != -1){
+    for(;;){
+      /* Seek for .mbn header, 32bit at once */
+      if(read(fd, (void*)&buf, sizeof buf) != sizeof(buf))
+	goto err;
+      /* 32bits read */
+      if(buf == MBN_HEAD_CODEWORD){
+	/* Now se seek for magic */
+	head->codeword = buf;
+	printf("codeword : 0x%08x\n", head->codeword);
+	if(read(fd, (void*)&buf, sizeof buf) != sizeof buf)
+	  goto err;
+	/* Antoher 32bits read*/
+	if(buf == MBN_HEAD_MAGIC){
+	  /* Found magic */
+	  head->magic = buf;
+	  printf("magic : 0x%08x\n", head->magic);
+	  int n = read(fd, (void*)&head->image_type, sizeof(*head)-8);
+	  if(n != sizeof(*head)-8)
+	    goto err;
+	  
+	  /* Read successfull */
+	  printf("Image type : %s\n",
+		 (head->image_type == MBN_HEAD_IMG_EHOSTDL ?
+		  "eHostDl" : "SBL1"));
+
+	  printf("Load address : 0x%08x\n", head->load_address);
+	  printf("Body length : %u\n", head->body_length);
+	  printf("Code length : %u\n", head->code_length);
+	  printf("Signature address : 0x%08x\n", head->signature_address);
+	  printf("Signature length : %u\n", head->signature_length);
+	  printf("Certificate address : 0x%08x\n", head->cert_store_address);
+	  printf("Certificate length : %u\n", head->cert_store_length);
+	  break;
+	}
+      }
+    }
+  }
+
+  return fd;
+
+  err:
+  /* Can't read data
+   * It can be either an I/O problem or the end of the file.
+   * We stop */
+  close(fd);
+  return -1;
+}
---
+++ mbn.h
@@ -0,0 +1,40 @@
+/*
+ * mbn.h
+ * dloadtool
+ *
+ * Created by Joachim Naulet on 3/14/2016
+ *
+ * Copyright (c) 2016 R&D Innovation. All rights reserved
+ */
+
+#ifndef MBN_H
+#define MBN_H
+
+/* 32-bit, little endian */
+typedef struct mbn_head {
+  uint32_t codeword;         /* d1 dC 4b 84 */
+  uint32_t magic;            /* 34 10 d7 73 */
+  uint32_t image_type;       /* see below */
+  uint32_t padding[2];       /* FFFFFFFF FFFFFFFF */
+  uint32_t head_length;      /* should be 50 */
+  uint32_t load_address;     /* load addr / start addr */
+  uint32_t body_length;      /* code + signature + certificater store size */
+  uint32_t code_length;
+  uint32_t signature_address;
+  uint32_t signature_length; /* 256 bytes */
+  uint32_t cert_store_address;
+  uint32_t cert_store_length;
+} __attribute__ ((packed)) mbn_head_t;
+
+/* Numbers */
+#define MBN_HEAD_CODEWORD 0x844bdcd1 /* little-endian */
+#define MBN_HEAD_MAGIC    0x73d71034 /* little-endian */
+
+/* Image type */
+#define MBN_HEAD_IMG_EHOSTDL 0x0d
+#define MBN_HEAD_IMG_SBL1    0x15
+
+/* Functions */
+int mbn_open(const char *path, mbn_head_t *head);
+
+#endif
